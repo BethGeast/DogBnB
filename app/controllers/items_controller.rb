de4d68bc2e34a9aab90699b8d0ad7fe06e3e0280
beth geast <beth.geast@gmail.com>
2024-03-12 20:04:57 +0000

    adds delete function, edit update new create and destroy only available to users signed in

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :set_item, only: %i[show edit update destroy]
-  skip_before_action :authenticate_user!, only: %i[index show edit update]
+  skip_before_action :authenticate_user!, only: %i[index show]
 
   def index
     @items = Item.all
