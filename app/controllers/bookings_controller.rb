d682d8f325ca01c15114209f883c9cc40edcc0c2
beth geast <beth.geast@gmail.com>
2024-03-14 15:19:02 +0000

    bookings controller complete with all paths

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,19 +1,46 @@
 class BookingsController < ApplicationController
-  before_action :set_item_bookings
+  before_action :set_item_bookings, only: %i[create]
 
   def new
-    @bookings = Booking.new
+    @booking = Booking.new
   end
 
   def create
-    @user = User.find(params[:user_id])
     @booking = Booking.new(booking_strong_params)
     @booking.item = @item
+    @booking.user = current_user
     if @booking.save
-      redirect_to item_path(@item)
+      redirect_to booking_path(@booking), notice: 'New booking successfully placed!'
     else
       render :new, status: :unprocessable_entity
     end
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+    @item = @booking.item
+  end
+
+  def edit
+    @booking = Booking.find(params[:id])
+    @item = @booking.item_id
+  end
+
+  def update
+    @booking = Booking.find(params[:id])
+    @item = @booking.item_id
+    @booking.update(booking_strong_params)
+    redirect_to bookings_path, notice: "Booking updated!"
+  end
+
+  def index
+    @bookings = Booking.all
+  end
+
+  def destroy
+    @booking = Booking.find(params[:id])
+    @booking.destroy
+    redirect_to bookings_path, status: :see_other, notice: "Booking has been cancelled"
   end
 
   private
@@ -23,6 +50,6 @@ class BookingsController < ApplicationController
   end
 
   def booking_strong_params
-    params.require(:booking).permit(:user_id, :item_id, :date_booked_start, :date_booked_end)
+    params.require(:booking).permit(:item_id, :date_booked_start, :date_booked_end)
   end
 end
