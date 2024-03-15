376d41a763129ab11f030fefff987d6616e39a43
beth geast <beth.geast@gmail.com>
2024-03-15 15:27:07 +0000

    seed file prepped

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,11 @@ require 'faker'
     name: Faker::Creature::Dog.meme_phrase,
     description: Faker::TvShows::MichaelScott.quote,
     price: Faker::Number.decimal_part(digits: 2),
-    date_available: Faker::Date.between(from: '2024-04-01', to: '2024-12-31'),
-    location: Faker::Address.city
+    date_available_from: Faker::Date.between(from: '2024-04-01', to: '2024-12-31'),
+    date_available_until: Faker::Date.between(from: '2024-04-01', to: '2024-12-31'),
+    location: Faker::Address.city,
+    max_guests: rand(1..10),
+    category: ["Big Dog", "Medium Dog", "Small Dog", "Multiple Dogs", "Woodland",
+      "Beachfront", "City", "Home-from-home"].sample
   )
 end
